1bf5afb574d01fb6e42d4cf74a4939a78008439d
SergioLarios <sergiolc@plugdata.io>
2019-06-06 07:34:41 +0200

    Added index.js to export all js in module

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
 
-export * from './types/src/shared-types';
+export * from './types';
---
+++ index.ts
@@ -0,0 +1,31 @@
+
+export * from './src/constants/plugdata.constants';
+export * from './src/constants/reflect-metadata.constants';
+export * from './src/constants/string.constants';
+
+export * from './src/dependecy-injection/di.interfaces';
+export * from './src/dependecy-injection/di.container';
+export * from './src/dependecy-injection/di.decorators';
+
+export * from './src/io/fs.utils';
+export * from './src/io/http.client';
+export * from './src/io/http.shared';
+export * from './src/io/http.utils';
+export * from './src/io/https.client';
+
+export * from './src/logs/log.enums';
+export * from './src/logs/log.interefaces';
+export * from './src/logs/logger';
+
+export * from './src/test/test.asserter';
+export * from './src/test/test.decorators';
+export * from './src/test/test.shared';
+
+export * from './src/utils/array.utils';
+export * from './src/utils/js-stack.utils';
+export * from './src/utils/log.utils';
+export * from './src/utils/object.utils';
+export * from './src/utils/promise.utils';
+export * from './src/utils/string.utils';
+export * from './src/utils/type.checker';
+export * from './src/utils/typescript.utils';
